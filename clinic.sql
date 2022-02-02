55dfcfc5c37510d7e15632b0266ce0a2a5f9c7c3
Omar Elsherif <omarelsherif010@gmail.com>
2022-02-02 08:54:59 +0200

    Initial commit

---
+++ clinic.sql
@@ -0,0 +1,5 @@
+#create database clinic
+#create table doctor(doc_id int primary key auto_increment,doc_name varchar(50),doc_nation varchar(14),doc_spec varchar(50),doc_gender varchar(10),doc_age int,doc_phone varchar(11),doc_address varchar(50))
+#insert into doctor (doc_name,doc_nation,doc_spec,doc_gender,doc_age,doc_phone,doc_address) values ("Ahmed",30107151401515,"Pediatrician","Male",30,01279827842,"Benha"),
+#("Taha","30105191826374","Endocrinologist","Male",32,01004651157,"Alex"),("Rawan","30107986345287","Neurologist","Female",26,01001918125,"Benha")
+select * from doctor
